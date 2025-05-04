449e5d6f5b89c429054201c0b377c9a042f67068
guard2929 <guardmaximus4@gmail.com>
2025-05-04 14:06:41 +0300

    1

--- pc_store/store/models.py
+++ pc_store/store/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 class CPU(models.Model):
     name = models.CharField(max_length=100)
