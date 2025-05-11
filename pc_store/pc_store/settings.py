c867de4450883c90f91811485b3723e3744ed4fb
guard2929 <guardmaximus4@gmail.com>
2025-05-11 13:59:57 +0300

    Update settings.py

--- pc_store/pc_store/settings.py
+++ pc_store/pc_store/settings.py
@@ -91,3 +91,11 @@ LOGGING = {
 } 
 LOGOUT_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'guardmaximus4@gmail.com'         
+EMAIL_HOST_PASSWORD = ''   
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
