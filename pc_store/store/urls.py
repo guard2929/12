75c4b50c98e04b88f2821691bc06aea7d78115e7
guard2929 <guardmaximus4@gmail.com>
2025-05-11 13:56:03 +0300

    Update urls.py

--- pc_store/store/urls.py
+++ pc_store/store/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('configure/', configure_pc, name='configure_pc'),
 path('profile/', views.profile_view, name='profile'),
 path('change-password/', views.change_password, name='change_password'),
+path('delete-account/', views.delete_account, name='delete_account'),
 
 ]
 
