3af384bb627b6e36f20530060e46a0bf3391e2d2
guard2929 <guardmaximus4@gmail.com>
2025-05-11 15:04:50 +0300

    1

--- pc_store/store/urls.py
+++ pc_store/store/urls.py
@@ -1,19 +1,18 @@
 from django.urls import path
-from .views import index, register, configure_pc
+from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('register/', register, name='register'),
+    path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='store/login.html'),name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
-    path('configure/', configure_pc, name='configure_pc'),
-path('profile/', views.profile_view, name='profile'),
-path('change-password/', views.change_password, name='change_password'),
-path('delete-account/', views.delete_account, name='delete_account'),
+    path('configure/', views.configure_pc, name='configure_pc'),
+    path('profile/', views.profile_view, name='profile'),
+    path('change-password/', views.change_password, name='change_password'),
+    path('delete-account/', views.delete_account, name='delete_account'),
 
 ]
-
 
 urlpatterns += [
     path('password-reset/',
