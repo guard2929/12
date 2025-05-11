e2504f6e6caa62d74dcab023fff1d3aeadd6f1c9
guard2929 <guardmaximus4@gmail.com>
2025-05-11 14:01:26 +0300

    Update urls.py

--- pc_store/store/urls.py
+++ pc_store/store/urls.py
@@ -14,3 +14,26 @@ path('delete-account/', views.delete_account, name='delete_account'),
 
 ]
 
+from django.contrib.auth import views as auth_views
+
+urlpatterns += [
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='store/password_reset.html',
+             email_template_name='store/password_reset_email.html',
+             subject_template_name='store/password_reset_subject.txt',
+         ), name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='store/password_reset_done.html'
+         ), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='store/password_reset_confirm.html'
+         ), name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='store/password_reset_complete.html'
+         ), name='password_reset_complete'),
+]
+
