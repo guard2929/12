dfdbc9f1bc26fdc1ec9aca6eef9628947ca6d2da
guard2929 <guardmaximus4@gmail.com>
2025-05-11 15:33:17 +0300

    1

--- pc_store/store/urls.py
+++ pc_store/store/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='store/login.html'),name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
     path('configure/', views.configure_pc, name='configure_pc'),
-    path('profile/', views.profile_view, name='profile'),
+    path('accounts/profile/', views.profile_view, name='profile'),
     path('change-password/', views.change_password, name='change_password'),
     path('delete-account/', views.delete_account, name='delete_account'),
 
