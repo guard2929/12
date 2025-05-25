0b4060bbe5e76a24b6a4f503b5f5364332548c59
guard2929 <guardmaximus4@gmail.com>
2025-05-25 14:15:54 +0300

    Update views.py

--- pc_store/store/views.py
+++ pc_store/store/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from .models import CPU, GPU, RAM, Storage, PCBuild, Product, PrebuiltOrder
-from .forms import RegisterForm, PCBuildForm
+from .forms import RegisterForm, PCBuildForm, ProductForm
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth import logout
