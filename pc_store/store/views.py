557a804d49386877948fda385248cf5b4178f5b1
guard2929 <guardmaximus4@gmail.com>
2025-05-25 14:11:42 +0300

    Update views.py

--- pc_store/store/views.py
+++ pc_store/store/views.py
@@ -1,3 +1,4 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
